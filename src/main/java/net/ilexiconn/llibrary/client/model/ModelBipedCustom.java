78b1f31730fe9ddaa7fade17eb41603a89e0bc6e
iLexiconn <ilexiconn@gmail.com>
2015-03-16 17:35:59 +0100

    Fixes

--- src/main/java/net/ilexiconn/llibrary/client/model/ModelBipedCustom.java
+++
@@ -1,8 +0,0 @@
-package net.ilexiconn.llibrary.client.model;
-
-import net.minecraft.client.model.ModelBiped;
-
-public class ModelBipedCustom extends ModelBiped
-{
-	
-}
---
+++ src/main/java/net/ilexiconn/llibrary/client/model/player/ModelCustomBiped.java
@@ -0,0 +1,8 @@
+package net.ilexiconn.llibrary.client.model.player;
+
+import net.minecraft.client.model.ModelBiped;
+
+public class ModelCustomBiped extends ModelBiped
+{
+
+}
