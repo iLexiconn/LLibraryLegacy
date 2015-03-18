489814555d0cdcac2325d889729af87f41ed9122
iLexiconn <ilexiconn@gmail.com>
2015-03-18 18:15:22 +0100

    Named the floats

--- src/main/java/net/ilexiconn/llibrary/LLibraryHooks.java
+++ src/main/java/net/ilexiconn/llibrary/LLibraryHooks.java
@@ -4,7 +4,7 @@ import net.minecraft.entity.Entity;
 
 public class LLibraryHooks
 {
-	public static void renderHook(Entity entity, float f1, float f2, float f3, float f4, float f5, float f6) //TODO Name those floats! :P
+	public static void renderHook(Entity entity, float limbSwing, float limbSwingAmount, float rotationFloat, float rotationYaw, float rotationPitch, float partialTicks)
 	{
 		System.out.println("It works!");
 	}
