6be244b86d48b59f7ab8eaa3567ac07a29a59943
iLexiconn <ilexiconn@gmail.com>
2015-03-18 18:14:04 +0100

    Huehuehue

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -1,29 +1,24 @@
 package net.ilexiconn.llibrary;
 
-import java.io.File;
-import java.lang.reflect.Field;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import net.ilexiconn.llibrary.command.CommandLLibrary;
-import net.ilexiconn.llibrary.proxy.ClientProxy;
-import net.ilexiconn.llibrary.proxy.ServerProxy;
-
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
-
 import cpw.mods.fml.client.FMLFileResourcePack;
 import cpw.mods.fml.client.FMLFolderResourcePack;
 import cpw.mods.fml.common.DummyModContainer;
 import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.ModMetadata;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.versioning.ArtifactVersion;
+import net.ilexiconn.llibrary.command.CommandLLibrary;
+import net.ilexiconn.llibrary.proxy.ClientProxy;
+import net.ilexiconn.llibrary.proxy.ServerProxy;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LLibrary extends DummyModContainer
 {
@@ -34,8 +29,8 @@ public class LLibrary extends DummyModContainer
 		meta.modId = "llibrary";
 		meta.name = "LLibrary";
 		meta.version = "${version}";
-		meta.credits = "";
-		meta.authorList = Arrays.asList("iLexiconn, gegy1000, FiskFille, BobMowzie, Rafa_MV");
+		meta.credits = "Gegy1000, FiskFille, BobMowzie, Rafa_MV";
+		meta.authorList = Arrays.asList("iLexiconn");
 		meta.description = "A lightweight library for Minecraft mods.";
 		meta.url = "https://ilexiconn.net/";
 		meta.updateUrl = "";
@@ -47,16 +42,13 @@ public class LLibrary extends DummyModContainer
 	}
 
 	public static ServerProxy proxy;
-
 	public static LLibrary instance;
 
-	@Override
 	public Class<?> getCustomResourcePackClass()
 	{
 		return getSource().isDirectory() ? FMLFolderResourcePack.class : FMLFileResourcePack.class;
 	}
 
-	@Override
 	public File getSource()
 	{
 		File modfile = LLibraryLoadingPlugin.modFile;
@@ -80,7 +72,6 @@ public class LLibrary extends DummyModContainer
 		}
 	}
 
-	@Override
 	public boolean registerBus(EventBus bus, LoadController controller)
 	{
 		bus.register(this);
@@ -108,11 +99,6 @@ public class LLibrary extends DummyModContainer
 	public void serverStart(FMLServerStartingEvent event)
 	{
 		event.registerServerCommand(new CommandLLibrary());
-	}
-	
-	@Subscribe
-	public void initialize(FMLInitializationEvent event)
-	{
 	}
 
 	@Subscribe
