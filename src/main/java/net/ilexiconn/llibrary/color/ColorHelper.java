ac8c9ec114aef97226243eec41f494b074d3569d
iLexiconn <ilexiconn@gmail.com>
2015-03-19 22:41:37 +0100

    Added color helper

---
+++ src/main/java/net/ilexiconn/llibrary/color/ColorHelper.java
@@ -0,0 +1,36 @@
+package net.ilexiconn.llibrary.color;
+
+/**
+ * Color helper for Minecraft guis.
+ *
+ * @author RafaMv
+ */
+public class ColorHelper
+{
+    /**
+     * Searches a color based on alpha, red, blue, and green parameters.
+     */
+    public static int getColorInt(int red_wanted, int blue_wanted, int green_wanted, int alpha_wanted)
+    {
+        int color = 0;
+        int red = 0;
+        int blue = 0;
+        int green = 0;
+        int alpha = 0;
+
+        while (alpha != alpha_wanted || red != red_wanted || green != green_wanted || blue != blue_wanted)
+        {
+            alpha = color >> 24 & 255;
+            red = color >> 16 & 255;
+            blue = color >> 8 & 255;
+            green = color & 255;
+
+            if (alpha < alpha_wanted) color += 50000;
+            else if (red < red_wanted) color += 500;
+            else if (blue < blue_wanted) color += 25;
+            else color += 1;
+        }
+
+        return color;
+    }
+}
