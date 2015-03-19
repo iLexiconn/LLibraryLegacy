555feb11e43e9e3a9f39f6a713b76fb3a559b78c
iLexiconn <ilexiconn@gmail.com>
2015-03-19 17:53:34 +0100

    For another day I guess

--- src/main/java/net/ilexiconn/llibrary/survivaltab/ISurvivalTab.java
+++ src/main/java/net/ilexiconn/llibrary/survivaltab/ISurvivalTab.java
@@ -1,5 +1,26 @@
 package net.ilexiconn.llibrary.survivaltab;
 
+import net.minecraft.item.ItemStack;
+
+/**
+ * Interface for custom survival tabs, register with TabRegistry.registerSurvivalTab().
+ *
+ * @author iLexiconn
+ */
 public interface ISurvivalTab
 {
+    /**
+     * @return unlocalized name string
+     */
+    public String getTabName();
+
+    /**
+     * @return itemstack with item or block to be displayed
+     */
+    public ItemStack getTabIcon();
+
+    /**
+     * Called when the survival tab is clicked.
+     */
+    public void openContainerGui();
 }
---
+++ src/main/java/net/ilexiconn/llibrary/survivaltab/TabRegistry.java
@@ -0,0 +1,9 @@
+package net.ilexiconn.llibrary.survivaltab;
+
+public class TabRegistry
+{
+    public static void registerSurvivalTab(ISurvivalTab survivalTab)
+    {
+        //todo: make the survival tab logic server-side
+    }
+}
