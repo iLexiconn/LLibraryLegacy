9aceb41139dcdafebdd8499981cfb1bc92e4cdd5
iLexiconn <ilexiconn@gmail.com>
2015-03-20 17:02:05 +0100

    Oops

--- src/main/java/test/proxy/ClientProxy.java
+++ src/main/java/test/proxy/ClientProxy.java
@@ -2,7 +2,6 @@ package test.proxy;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.ilexiconn.llibrary.client.render.RenderHelper;
-import net.minecraft.client.model.ModelBiped;
 import test.EntityTest;
 import test.render.RenderEntityTest;
 import test.render.TestModelExtension;
@@ -12,6 +11,6 @@ public class ClientProxy extends ServerProxy
     public void init()
     {
         RenderingRegistry.registerEntityRenderingHandler(EntityTest.class, new RenderEntityTest());
-        RenderHelper.registerModelExtension(ModelBiped.class, new TestModelExtension());
+        RenderHelper.registerModelExtension(new TestModelExtension());
     }
 }
