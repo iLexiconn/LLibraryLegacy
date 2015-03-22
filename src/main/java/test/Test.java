6ce000441a022060c2c6b0de83020ebf9e6b1d04
iLexiconn <ilexiconn@gmail.com>
2015-03-22 12:55:58 +0100

    Stuff

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -6,11 +6,14 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.ilexiconn.llibrary.ContentHandlerList;
 import net.ilexiconn.llibrary.IContentHandler;
+import net.ilexiconn.llibrary.creativetab.CreativeTabSearch;
 import net.ilexiconn.llibrary.entity.EntityHelper;
 import net.ilexiconn.llibrary.item.ItemHelper;
 import net.ilexiconn.llibrary.update.UpdateHelper;
 import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import test.proxy.ServerProxy;
 
 @Mod(modid = "test", name = "Test Mod", version = "1.0")
@@ -24,6 +27,13 @@ public class Test implements IContentHandler
     {
         ContentHandlerList.createList(this).init();
         UpdateHelper.registerUpdateChecker(this, "r5zKe0UF", "https://twitter.com/");
+        new CreativeTabSearch("test")
+        {
+            public Item getTabIconItem()
+            {
+                return Items.bone;
+            }
+        };
     }
 
     @Mod.EventHandler
