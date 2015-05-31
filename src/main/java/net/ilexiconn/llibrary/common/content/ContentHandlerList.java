6e5918bc6f7785d616def78a287015d73d6b3aa1
iLexiconn <iLexiconn@gmail.com>
2015-05-31 11:07:41 +0200

    Added IClientOnlyHandler to register content client-only, and made the new ContentHelper to register IContentHandlers

--- src/main/java/net/ilexiconn/llibrary/common/content/ContentHandlerList.java
+++ src/main/java/net/ilexiconn/llibrary/common/content/ContentHandlerList.java
@@ -4,22 +4,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * ArrayList to handle IContentHandlers
+ * ArrayList to handle IContentHandlers. Use {@link net.ilexiconn.llibrary.common.content.ContentHelper} instead.
  *
  * @author iLexiconn
  * @see net.ilexiconn.llibrary.common.content.IContentHandler
  * @since 0.1.0
  */
+@Deprecated
 public class ContentHandlerList extends ArrayList<IContentHandler>
 {
     /**
-     * Create a new list with IContentHandlers
+     * Create a new list with IContentHandlers. Use {@link net.ilexiconn.llibrary.common.content.ContentHelper#init(IContentHandler...)} instead.
      *
      * @param contentHandlers the list of IContentHandlers
      * @return a new instance of the list
      * @see net.ilexiconn.llibrary.common.content.IContentHandler
      * @since 0.1.0
      */
+    @Deprecated
     public static ContentHandlerList createList(IContentHandler... contentHandlers)
     {
         ContentHandlerList list = new ContentHandlerList();
@@ -28,24 +30,14 @@ public class ContentHandlerList extends ArrayList<IContentHandler>
     }
 
     /**
-     * Initialize all the IContentHandlers in this list.
+     * Initialize all the IContentHandlers in this list. Use {@link net.ilexiconn.llibrary.common.content.ContentHelper#init(IContentHandler...)} instead.
      *
      * @see net.ilexiconn.llibrary.common.content.IContentHandler
      * @since 0.1.0
      */
+    @Deprecated
     public void init()
     {
-        for (IContentHandler contentHandler : this)
-        {
-            try
-            {
-                contentHandler.init();
-                contentHandler.gameRegistry();
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
+        ContentHelper.init(toArray(new DummyContentHandler[size()]));
     }
 }
---
+++ src/main/java/net/ilexiconn/llibrary/common/content/DummyContentHandler.java
@@ -0,0 +1,14 @@
+package net.ilexiconn.llibrary.common.content;
+
+public class DummyContentHandler implements IContentHandler
+{
+    public void init()
+    {
+
+    }
+
+    public void gameRegistry() throws Exception
+    {
+
+    }
+}
