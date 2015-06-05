317350829afff678fc5b10a497c5fcbddf167d43
Gerard Bruwn <siribby@outlook.com>
2015-06-05 08:26:31 -0700

    Renamed versions.txt to versions.json

--- src/main/java/net/ilexiconn/llibrary/common/ServerProxy.java
+++ src/main/java/net/ilexiconn/llibrary/common/ServerProxy.java
@@ -19,7 +19,7 @@ public class ServerProxy
 
         try
         {
-            UpdateHelper.registerUpdateChecker(LLibrary.instance, "https://github.com/iLexiconn/LLibrary/raw/1.7.10/versions.txt");
+            UpdateHelper.registerUpdateChecker(LLibrary.instance, "https://github.com/iLexiconn/LLibrary/raw/1.7.10/versions.json");
         }
         catch (IOException e)
         {
