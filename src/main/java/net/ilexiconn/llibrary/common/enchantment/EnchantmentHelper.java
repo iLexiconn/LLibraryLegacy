3e238a31458b24b852a7cdeae7dfb74695854534
iLexiconn <ilexiconn@gmail.com>
2015-06-07 10:14:14 +0200

    Fixed formatting

--- src/main/java/net/ilexiconn/llibrary/common/enchantment/EnchantmentHelper.java
+++ src/main/java/net/ilexiconn/llibrary/common/enchantment/EnchantmentHelper.java
@@ -3,20 +3,23 @@ package net.ilexiconn.llibrary.common.enchantment;
 import net.minecraft.enchantment.Enchantment;
 
 /**
- * @Author JTGhawk137
+ * @author JTGhawk137
  * @since 0.2.1
  */
-public class EnchantmentHelper {
-
+public class EnchantmentHelper 
+{
     /**
      * Removes the specified enchantment from minecraft
-     * @param enchantment
+     * 
+     * @since 0.2.1
      */
     public static void removeEnchantment(Enchantment enchantment)
     {
         Enchantment[] ench = Enchantment.enchantmentsList;
-        for(int i = 0; i < ench.length; i++) {
-            if(ench[i] == enchantment) {
+        for (int i = 0; i < ench.length; i++) 
+        {
+            if (ench[i] == enchantment) 
+            {
                 ench[i] = null;
             }
         }
