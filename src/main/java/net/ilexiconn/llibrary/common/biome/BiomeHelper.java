5ed8186d5675b13c7cfa150a645f5dd73d3568e8
gegy1000 <gegy1000@gmail.com>
2015-06-10 18:37:02 +0200

    Some more stuff for BiomeHelper

--- src/main/java/net/ilexiconn/llibrary/common/biome/BiomeHelper.java
+++ src/main/java/net/ilexiconn/llibrary/common/biome/BiomeHelper.java
@@ -9,7 +9,31 @@ import net.minecraft.world.biome.BiomeGenBase;
 public class BiomeHelper
 {
     /**
+     * @param oldBiomeId the id of the biome to replace
+     * @param newBiome the biome to replace the current one with
+     * 
+     * @since 0.3.0
+     */
+    public static void overrideBiome(int oldBiomeId, BiomeGenBase newBiome)
+    {
+        BiomeGenBase.getBiomeGenArray()[oldBiomeId] = newBiome;
+    }
+    
+    /**
+     * @param oldBiome the biome to replace
+     * @param newBiome the biome to replace the current one with
+     * 
+     * @since 0.3.0
+     */
+    public static void overrideBiome(BiomeGenBase oldBiome, BiomeGenBase newBiome)
+    {
+        overrideBiome(oldBiome.biomeID, newBiome);
+    }
+
+    /**
      * Creates a unique biome id, not used by any other biome.
+     * 
+     * @since 0.3.0
      */
     public static int getUniqueBiomeId()
     {
