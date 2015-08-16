5a31206e06c7a6de1d601b8e8dbc6502feda268a
iLexiconn <ilexiconn@gmail.com>
2015-08-16 23:11:10 +0200

    Splitting LLoader to a separate mod

---
+++ src/main/java/net/ilexiconn/lloader/log/LoggerHelper.java
@@ -0,0 +1,59 @@
+package net.ilexiconn.lloader.log;
+
+import net.minecraftforge.fml.common.FMLLog;
+import org.apache.logging.log4j.Level;
+
+/**
+ * Class to help with usage of Log4J2
+ *
+ * @author Ry_dog101
+ */
+public class LoggerHelper
+{
+    private String modid;
+
+    public LoggerHelper(String modid)
+    {
+        this.modid = modid;
+    }
+
+    private void log(Level logLevel, Object object)
+    {
+        FMLLog.log(modid, logLevel, String.valueOf(object));
+    }
+
+    public void debug(Object object)
+    {
+        log(Level.DEBUG, object);
+    }
+
+    public void error(Object object)
+    {
+        log(Level.ERROR, object);
+    }
+
+    public void fatal(Object object)
+    {
+        log(Level.FATAL, object);
+    }
+
+    public void info(Object object)
+    {
+        log(Level.INFO, object);
+    }
+
+    public void off(Object object)
+    {
+        log(Level.OFF, object);
+    }
+
+    public void trace(Object object)
+    {
+        log(Level.TRACE, object);
+    }
+
+    public void warn(Object object)
+    {
+        log(Level.WARN, object);
+    }
+}
