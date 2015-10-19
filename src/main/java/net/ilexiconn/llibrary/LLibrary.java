0ff5742f1e6c24b025a41df71551b57b508caeda
iLexiconn <ilexiconn@gmail.com>
2015-10-19 21:14:45 +0200

    Released 0.4.3

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -23,7 +23,7 @@ import net.minecraft.crash.CrashReport;
 
 import java.util.Map;
 
-@Mod(modid = "llibrary", name = "LLibrary", version = "0.4.2", guiFactory = "net.ilexiconn.llibrary.client.gui.GuiLLibraryConfigFactory")
+@Mod(modid = "llibrary", name = "LLibrary", version = "0.4.3", guiFactory = "net.ilexiconn.llibrary.client.gui.GuiLLibraryConfigFactory")
 public class LLibrary
 {
     @Mod.Instance("llibrary")
