a11ecdf6c1a27a72468cb07d7f40ebeb0c45a2cb
iLexiconn <ilexiconn@gmail.com>
2015-11-15 19:36:23 +0100

    Added OBJ loader

---
+++ src/main/java/net/ilexiconn/llibrary/client/model/obj/IModelCustom.java
@@ -0,0 +1,20 @@
+package net.ilexiconn.llibrary.client.model.obj;
+
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public interface IModelCustom {
+    String getType();
+
+    @SideOnly(Side.CLIENT)
+    void renderAll();
+
+    @SideOnly(Side.CLIENT)
+    void renderOnly(String... groupNames);
+
+    @SideOnly(Side.CLIENT)
+    void renderPart(String partName);
+
+    @SideOnly(Side.CLIENT)
+    void renderAllExcept(String... excludedGroupNames);
+}
