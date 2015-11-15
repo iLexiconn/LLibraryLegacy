66d0dc2ef8b2181e7bd25f0b4e0eab879938dc17
iLexiconn <ilexiconn@gmail.com>
2015-11-15 22:57:53 +0100

    Added command argument types

---
+++ src/main/java/net/ilexiconn/llibrary/common/command/ArgumentType.java
@@ -0,0 +1,53 @@
+package net.ilexiconn.llibrary.common.command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.command.PlayerNotFoundException;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+
+public abstract class ArgumentType<T> {
+    public static ArgumentType<String> STRING = new ArgumentType<String>() {
+        @Override
+        public String parse(ICommandSender sender, String argument) throws CommandException {
+            return argument;
+        }
+    };
+
+    public static ArgumentType<Integer> INT = new ArgumentType<Integer>() {
+        @Override
+        public Integer parse(ICommandSender sender, String argument) throws CommandException {
+            return CommandBase.parseInt(argument);
+        }
+    };
+
+    public static ArgumentType<EntityPlayer> PLAYER = new ArgumentType<EntityPlayer>() {
+        @Override
+        public EntityPlayer parse(ICommandSender sender, String argument) throws CommandException {
+            EntityPlayer player = MinecraftServer.getServer().getConfigurationManager().getPlayerByUsername(argument);
+            if (player == null) {
+                throw new PlayerNotFoundException();
+            } else {
+                return player;
+            }
+        }
+    };
+
+    public static ArgumentType<ItemStack> STACK = new ArgumentType<ItemStack>() {
+        @Override
+        public ItemStack parse(ICommandSender sender, String argument) throws CommandException {
+            return new ItemStack(CommandBase.getItemByText(sender, argument));
+        }
+    };
+
+    public static ArgumentType<Boolean> BOOLEAN = new ArgumentType<Boolean>() {
+        @Override
+        public Boolean parse(ICommandSender sender, String argument) throws CommandException {
+            return CommandBase.parseBoolean(argument);
+        }
+    };
+
+    public abstract T parse(ICommandSender sender, String argument) throws CommandException;
+}
--- src/main/java/net/ilexiconn/llibrary/common/world/gen/ChunkProviderHeightmap.java
+++ src/main/java/net/ilexiconn/llibrary/common/world/gen/ChunkProviderHeightmap.java
@@ -25,62 +25,60 @@ import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.SCAT
 import static net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate.EventType.ANIMALS;
 import static net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate.EventType.ICE;
 
-public class ChunkProviderCustom implements IChunkProvider
-{
-    /** RNG. */
+public class ChunkProviderHeightmap implements IChunkProvider {
+    /**
+     * RNG.
+     */
     private Random rand;
-    /** Reference to the World object. */
+    /**
+     * Reference to the World object.
+     */
     private World worldObj;
     private MapGenScatteredFeature scatteredFeatureGenerator;
-    /** The biomes that are used to generate the chunk */
+    /**
+     * The biomes that are used to generate the chunk
+     */
     private BiomeGenBase[] biomesForGeneration;
 
     private WorldHeightmapGenerator heightmapGenerator;
 
-    public ChunkProviderCustom(World world, long seed, WorldHeightmapGenerator heightmapGenerator)
-    {
+    public ChunkProviderHeightmap(World world, long seed, WorldHeightmapGenerator heightmapGenerator) {
         this.scatteredFeatureGenerator = new MapGenScatteredFeature();
-        scatteredFeatureGenerator = (MapGenScatteredFeature)TerrainGen.getModdedMapGen(scatteredFeatureGenerator, SCATTERED_FEATURE);
+        scatteredFeatureGenerator = (MapGenScatteredFeature) TerrainGen.getModdedMapGen(scatteredFeatureGenerator, SCATTERED_FEATURE);
         this.worldObj = world;
         this.rand = new Random(seed);
         this.heightmapGenerator = heightmapGenerator;
 
-        if (!heightmapGenerator.isLoaded())
-        {
+        if (!heightmapGenerator.isLoaded()) {
             heightmapGenerator.load(); //TODO add capability to specify what you want to generate, eg ravines, caves etc
         }
     }
 
-    public void setBlocksInChunk(int chunkX, int chunkZ, ChunkPrimer chunkPrimer)
-    {
+    public void setBlocksInChunk(int chunkX, int chunkZ, ChunkPrimer chunkPrimer) {
         this.biomesForGeneration = this.worldObj.getWorldChunkManager().getBiomesForGeneration(this.biomesForGeneration, chunkX * 4 - 2, chunkZ * 4 - 2, 10, 10);
 
-        for (int x = 0; x < 16; x++)
-        {
-            for (int z = 0; z < 16; z++)
-            {
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
                 int height = (heightmapGenerator.getHeightForCoords(x + (chunkX * 16), z + (chunkZ * 16)));
 
-                for (int y = 0; y < height; y++)
-                {
+                for (int y = 0; y < height; y++) {
                     chunkPrimer.setBlockState(x, y, z, heightmapGenerator.getStoneBlock());
                 }
             }
         }
     }
 
-    public void func_180517_a(int chunkX, int chunkZ, ChunkPrimer chunkPrimer, BiomeGenBase[] biomes)
-    {
+    public void func_180517_a(int chunkX, int chunkZ, ChunkPrimer chunkPrimer, BiomeGenBase[] biomes) {
         ChunkProviderEvent.ReplaceBiomeBlocks event = new ChunkProviderEvent.ReplaceBiomeBlocks(this, chunkX, chunkZ, chunkPrimer, this.worldObj);
         MinecraftForge.EVENT_BUS.post(event);
-        if (event.getResult() == Result.DENY) return;
+        if (event.getResult() == Result.DENY) {
+            return;
+        }
 
         double d0 = 0.03125D;
 
-        for (int k = 0; k < 16; ++k)
-        {
-            for (int l = 0; l < 16; ++l)
-            {
+        for (int k = 0; k < 16; ++k) {
+            for (int l = 0; l < 16; ++l) {
                 BiomeGenBase biome = biomes[l + k * 16];
                 biome.genTerrainBlocks(this.worldObj, this.rand, chunkPrimer, chunkX * 16 + k, chunkZ * 16 + l, 0);
             }
@@ -91,9 +89,8 @@ public class ChunkProviderCustom implements IChunkProvider
      * Will return back a chunk, if it doesn't exist and its not a MP client it will generates all the blocks for the
      * specified chunk from the map seed and chunk seed
      */
-    public Chunk provideChunk(int x, int z)
-    {
-        this.rand.setSeed((long)x * 341873128712L + (long) z * 132897987541L);
+    public Chunk provideChunk(int x, int z) {
+        this.rand.setSeed((long) x * 341873128712L + (long) z * 132897987541L);
         ChunkPrimer chunkprimer = new ChunkPrimer();
         this.setBlocksInChunk(x, z, chunkprimer);
         this.biomesForGeneration = this.worldObj.getWorldChunkManager().loadBlockGeneratorData(this.biomesForGeneration, x * 16, z * 16, 16, 16);
@@ -105,8 +102,9 @@ public class ChunkProviderCustom implements IChunkProvider
 
         //Set biomes
 
-        for (int biomeIndex = 0; biomeIndex < biomeArray.length; ++biomeIndex)
-            biomeArray[biomeIndex] = (byte)this.biomesForGeneration[biomeIndex].biomeID;
+        for (int biomeIndex = 0; biomeIndex < biomeArray.length; ++biomeIndex) {
+            biomeArray[biomeIndex] = (byte) this.biomesForGeneration[biomeIndex].biomeID;
+        }
 
         chunk.generateSkylightMap();
         return chunk;
@@ -115,16 +113,14 @@ public class ChunkProviderCustom implements IChunkProvider
     /**
      * Checks to see if a chunk exists at x, z
      */
-    public boolean chunkExists(int x, int z)
-    {
+    public boolean chunkExists(int x, int z) {
         return true;
     }
 
     /**
      * Populates chunk with ores etc etc
      */
-    public void populate(IChunkProvider provider, int p_73153_2_, int p_73153_3_)
-    {
+    public void populate(IChunkProvider provider, int p_73153_2_, int p_73153_3_) {
         BlockFalling.fallInstantly = true;
         int k = p_73153_2_ * 16;
         int l = p_73153_3_ * 16;
@@ -133,7 +129,7 @@ public class ChunkProviderCustom implements IChunkProvider
         this.rand.setSeed(this.worldObj.getSeed());
         long i1 = this.rand.nextLong() / 2L * 2L + 1L;
         long j1 = this.rand.nextLong() / 2L * 2L + 1L;
-        this.rand.setSeed((long)p_73153_2_ * i1 + (long)p_73153_3_ * j1 ^ this.worldObj.getSeed());
+        this.rand.setSeed((long) p_73153_2_ * i1 + (long) p_73153_3_ * j1 ^ this.worldObj.getSeed());
         boolean flag = false;
 
         MinecraftForge.EVENT_BUS.post(new PopulateChunkEvent.Pre(provider, worldObj, rand, p_73153_2_, p_73153_3_, flag));
@@ -143,27 +139,22 @@ public class ChunkProviderCustom implements IChunkProvider
         int i2;
 
         biomegenbase.decorate(this.worldObj, this.rand, new BlockPos(k, 0, l));
-        if (TerrainGen.populate(provider, worldObj, rand, p_73153_2_, p_73153_3_, flag, ANIMALS))
-        {
+        if (TerrainGen.populate(provider, worldObj, rand, p_73153_2_, p_73153_3_, flag, ANIMALS)) {
             SpawnerAnimals.performWorldGenSpawning(this.worldObj, biomegenbase, k + 8, l + 8, 16, 16, this.rand);
         }
         blockpos = blockpos.add(8, 0, 8);
 
         boolean doGen = TerrainGen.populate(provider, worldObj, rand, p_73153_2_, p_73153_3_, flag, ICE);
-        for (k1 = 0; doGen && k1 < 16; ++k1)
-        {
-            for (l1 = 0; l1 < 16; ++l1)
-            {
+        for (k1 = 0; doGen && k1 < 16; ++k1) {
+            for (l1 = 0; l1 < 16; ++l1) {
                 BlockPos blockpos1 = this.worldObj.getPrecipitationHeight(blockpos.add(k1, 0, l1));
                 BlockPos blockpos2 = blockpos1.down();
 
-                if (this.worldObj.func_175675_v(blockpos2))
-                {
+                if (this.worldObj.func_175675_v(blockpos2)) {
                     this.worldObj.setBlockState(blockpos2, Blocks.ice.getDefaultState(), 2);
                 }
 
-                if (this.worldObj.canSnowAt(blockpos1, true))
-                {
+                if (this.worldObj.canSnowAt(blockpos1, true)) {
                     this.worldObj.setBlockState(blockpos1, Blocks.snow_layer.getDefaultState(), 2);
                 }
             }
@@ -173,8 +164,7 @@ public class ChunkProviderCustom implements IChunkProvider
         BlockFalling.fallInstantly = false;
     }
 
-    public boolean func_177460_a(IChunkProvider provider, Chunk chunk, int x, int y)
-    {
+    public boolean func_177460_a(IChunkProvider provider, Chunk chunk, int x, int y) {
         return false;
     }
 
@@ -182,8 +172,7 @@ public class ChunkProviderCustom implements IChunkProvider
      * Two modes of operation: if passed true, save all Chunks in one go.  If passed false, save up to two chunks.
      * Return true if all chunks have been saved.
      */
-    public boolean saveChunks(boolean saveAll, IProgressUpdate progressUpdate)
-    {
+    public boolean saveChunks(boolean saveAll, IProgressUpdate progressUpdate) {
         return true;
     }
 
@@ -191,53 +180,46 @@ public class ChunkProviderCustom implements IChunkProvider
      * Save extra data not associated with any Chunk.  Not saved during autosave, only during world unload.  Currently
      * unimplemented.
      */
-    public void saveExtraData() {}
+    public void saveExtraData() {
+    }
 
     /**
      * Unloads chunks that are marked to be unloaded. This is not guaranteed to unload every such chunk.
      */
-    public boolean unloadQueuedChunks()
-    {
+    public boolean unloadQueuedChunks() {
         return false;
     }
 
     /**
      * Returns if the IChunkProvider supports saving.
      */
-    public boolean canSave()
-    {
+    public boolean canSave() {
         return true;
     }
 
     /**
      * Converts the instance data to a readable string.
      */
-    public String makeString()
-    {
+    public String makeString() {
         return "RandomLevelSource";
     }
 
-    public List func_177458_a(EnumCreatureType creatureType, BlockPos pos)
-    {
+    public List func_177458_a(EnumCreatureType creatureType, BlockPos pos) {
         return worldObj.getBiomeGenForCoords(pos).getSpawnableList(creatureType);
     }
 
-    public BlockPos getStrongholdGen(World world, String gen, BlockPos pos)
-    {
+    public BlockPos getStrongholdGen(World world, String gen, BlockPos pos) {
         return pos;
     }
 
-    public int getLoadedChunkCount()
-    {
+    public int getLoadedChunkCount() {
         return 0;
     }
 
-    public void recreateStructures(Chunk chunk, int x, int z)
-    {
+    public void recreateStructures(Chunk chunk, int x, int z) {
     }
 
-    public Chunk provideChunk(BlockPos pos)
-    {
+    public Chunk provideChunk(BlockPos pos) {
         return this.provideChunk(pos.getX() >> 4, pos.getZ() >> 4);
     }
 }
