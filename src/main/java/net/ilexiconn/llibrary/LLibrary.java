9feb603d01aecc1c398951d774b3ddd3a7474d30
iLexiconn <ilexiconn@gmail.com>
2015-11-24 18:46:38 +0100

    Fixed version

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -31,7 +31,7 @@ public class LLibrary {
     public static LoggerHelper logger = new LoggerHelper("llibrary");
     public static SimpleNetworkWrapper networkWrapper;
 
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.6.0";
 
     public static SurvivalTab tabInventory = SurvivalTab.create("container.inventory").setIcon(new ItemStack(Items.diamond_sword));
 
