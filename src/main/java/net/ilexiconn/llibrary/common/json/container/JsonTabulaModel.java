eda3194ea2094171ae1b5e72641b93ada0a5e59e
iLexiconn <ilexiconn@gmail.com>
2015-12-05 11:26:44 +0100

    Hopefully fix #56

--- src/main/java/net/ilexiconn/llibrary/common/json/container/JsonTabulaModel.java
+++ src/main/java/net/ilexiconn/llibrary/common/json/container/JsonTabulaModel.java
@@ -20,23 +20,11 @@ public class JsonTabulaModel {
 
     public final double[] scale = new double[]{1d, 1d, 1d};
 
-    public final ArrayList<CubeGroup> cubeGroups;
-    public final ArrayList<CubeInfo> cubes;
-    public final ArrayList<Animation> anims;
+    public ArrayList<CubeGroup> cubeGroups;
+    public ArrayList<CubeInfo> cubes;
+    public ArrayList<Animation> anims;
 
-    public final int cubeCount;
-
-    @Deprecated
-    public JsonTabulaModel() {
-        this(null, null, null, 0);
-    }
-
-    public JsonTabulaModel(ArrayList<CubeGroup> cubeGroups, ArrayList<CubeInfo> cubes, ArrayList<Animation> anims, int cubeCount) {
-        this.cubeGroups = cubeGroups;
-        this.cubes = cubes;
-        this.anims = anims;
-        this.cubeCount = cubeCount;
-    }
+    public int cubeCount;
 
     public int getTextureWidth() {
         return textureWidth;
