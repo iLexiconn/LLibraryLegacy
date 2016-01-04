6e49ede22633ef26f2901f2bb36d6080691b3afd
iLexiconn <ilexiconn@gmail.com>
2016-01-04 19:34:19 +0100

    Released 0.7.0

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -33,7 +33,7 @@ public class LLibrary {
     public static LoggerHelper logger = new LoggerHelper("llibrary");
     public static SimpleNetworkWrapper networkWrapper;
 
-    public static final String VERSION = "0.7.0-develop";
+    public static final String VERSION = "0.7.0";
 
     @Mod.EventHandler
     private void preInit(FMLPreInitializationEvent event) {
--- src/main/java/net/ilexiconn/llibrary/common/asm/HookPatchManager.java
+++ src/main/java/net/ilexiconn/llibrary/common/asm/HookPatchManager.java
@@ -68,52 +68,51 @@ public class HookPatchManager implements IClassTransformer {
     }
 
     public void transform(MethodNode methodNode, JsonHook hook) {
-        for (int i = 0; i < methodNode.instructions.size(); i++) {
-            AbstractInsnNode insnNode = methodNode.instructions.get(i);
-            if (insnNode.getOpcode() == Opcodes.GETSTATIC) {
-                InsnList insnList = new InsnList();
-                String desc = "(";
-                if (hook.hookClass.addClassAsArg) {
-                    desc += "L" + hook.hookClass.name + ";";
-                    insnList.add(new VarInsnNode(Opcodes.ALOAD, 0));
-                }
-                int j = 0;
-                for (String s : hook.hookMethod.args) {
-                    if (s.equals("int")) {
-                        desc += "I";
-                        insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
-                    } else if (s.equals("boolean")) {
-                        desc += "Z";
-                        insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
-                    } else if (s.equals("float")) {
-                        desc += "F";
-                        insnList.add(new VarInsnNode(Opcodes.FLOAD, j++));
-                    } else if (s.equals("double")) {
-                        desc += "D";
-                        insnList.add(new VarInsnNode(Opcodes.DLOAD, j++));
-                    } else if (s.equals("long")) {
-                        desc += "J";
-                        insnList.add(new VarInsnNode(Opcodes.LLOAD, j++));
-                    } else if (s.equals("byte")) {
-                        desc += "B";
-                        insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
-                    } else if (s.equals("char")) {
-                        desc += "C";
-                        insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
-                    } else if (s.equals("string")) {
-                        desc += "Ljava/lang/String;";
-                        insnList.add(new VarInsnNode(Opcodes.ALOAD, j++));
-                    } else {
-                        desc += "L" + s + ";";
-                        insnList.add(new VarInsnNode(Opcodes.ALOAD, j++));
-                    }
-                }
-                desc += ")V";
-                insnList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, hook.callerClass, hook.hookMethod.name, Mappings.getClassMappings(desc), false));
-                methodNode.instructions.insertBefore(insnNode, insnList);
-                break;
+        InsnList insnList = new InsnList();
+        String desc = "(";
+        if (hook.hookClass.addClassAsArg) {
+            desc += "L" + hook.hookClass.name + ";";
+            insnList.add(new VarInsnNode(Opcodes.ALOAD, 0));
+        }
+        int j = 1;
+        for (String s : hook.hookMethod.args) {
+            if (s.equals("int")) {
+                desc += "I";
+                insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
+            } else if (s.equals("boolean")) {
+                desc += "Z";
+                insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
+            } else if (s.equals("float")) {
+                desc += "F";
+                insnList.add(new VarInsnNode(Opcodes.FLOAD, j++));
+            } else if (s.equals("double")) {
+                desc += "D";
+                insnList.add(new VarInsnNode(Opcodes.DLOAD, j++));
+            } else if (s.equals("long")) {
+                desc += "J";
+                insnList.add(new VarInsnNode(Opcodes.LLOAD, j++));
+            } else if (s.equals("byte")) {
+                desc += "B";
+                insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
+            } else if (s.equals("char")) {
+                desc += "C";
+                insnList.add(new VarInsnNode(Opcodes.ILOAD, j++));
+            } else if (s.equals("string")) {
+                desc += "Ljava/lang/String;";
+                insnList.add(new VarInsnNode(Opcodes.ALOAD, j++));
+            } else {
+                desc += "L" + s + ";";
+                insnList.add(new VarInsnNode(Opcodes.ALOAD, j++));
             }
         }
+        desc += ")V";
+        insnList.add(new MethodInsnNode(Opcodes.INVOKESTATIC, hook.callerClass, hook.hookMethod.name, Mappings.getClassMappings(desc), false));
+        /*LabelNode labelNode = new LabelNode();
+        insnList.add(new JumpInsnNode(Opcodes.IFEQ, labelNode));
+        insnList.add(new InsnNode(Opcodes.RETURN));
+        insnList.add(labelNode);
+        insnList.add(new FrameNode(Opcodes.F_SAME, 0, null, 0, null));*/
+        methodNode.instructions.insertBefore(methodNode.instructions.get(0), insnList);
     }
 
     public static ClassNode createClassFromByteArray(byte[] classBytes) {
