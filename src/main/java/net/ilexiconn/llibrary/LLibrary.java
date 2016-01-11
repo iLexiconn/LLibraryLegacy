79565a53cd0aea9e7ee3ac897ff18da61499c6b6
iLexiconn <ilexiconn@gmail.com>
2016-01-11 23:25:21 +0100

    Updates

--- src/main/java/net/ilexiconn/llibrary/LLibrary.java
+++ src/main/java/net/ilexiconn/llibrary/LLibrary.java
@@ -33,7 +33,7 @@ public class LLibrary {
     public static LoggerHelper logger = new LoggerHelper("llibrary");
     public static SimpleNetworkWrapper networkWrapper;
 
-    public static final String VERSION = "0.7.1";
+    public static final String VERSION = "0.7.2";
 
     @Mod.EventHandler
     private void preInit(FMLPreInitializationEvent event) {
